64764846b02805e8aaac607ec60e38cbc86ad988
Telebird <tellen@ya.ru>
2023-04-23 15:01:53 +0400

    example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,10 +11,10 @@
 </script>
 
 <template>
-    <h1>{{ msg }}</h1>
-
+    <br /><br /><br /><br /><br />
     <a href="#" @click.prevent="show = !show">Show / Hide</a>
-    <SmoothFade v-model="show">
+    <SmoothFade v-model="show" :duration="1000">
+        <h1>{{ msg }}</h1>
         <p>
             Check out
             <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite starter
@@ -29,10 +29,6 @@
 
     <div class="card">
         <button type="button" @click="count++">count is {{ count }}</button>
-        <p>
-            Edit
-            <code>components/HelloWorld.vue</code> to test HMR
-        </p>
     </div>
 </template>
 
